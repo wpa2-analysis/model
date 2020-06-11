1a6d2fdf017266c660691898bce98ee2bb43b8e2
Anonymous WPA2 User <anonymous-wpa2@anonymous-wpa2.com>
2020-06-11 15:51:17 +0200

    Modified proof-generation script.

--- proof_generation/generate_proofs.sh
+++ proof_generation/generate_proofs.sh
@@ -12,7 +12,7 @@ if ! [[ -e ./proof_generation/ut_tamarin/ut_tamarin ]]; then
 fi
 echo === Running Tamarin on Lemmas ===
 echo 
-tamarin_file="wpa2_four_way_handshake.spthy"
+tamarin_file="model/wpa2_four_way_handshake_patched.spthy"
 ut_tamarin="./proof_generation/ut_tamarin/ut_tamarin"
 $ut_tamarin $tamarin_file --timeout=15000 --config_file=./proof_generation/utt_config.json --proof_directory=automatic_proofs ${@:1}
 proof_generation/ku_proofs.sh
